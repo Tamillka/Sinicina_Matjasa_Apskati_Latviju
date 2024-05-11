f4f28b77af0ab5066a187fc342824de577613ea9
Tamillka <tamila.matjasha@gmail.com>
2024-05-11 22:12:02 +0300

    jauna sekcija
    Kopīgi izdarījam jauno sekciju ar kontaktiem un modifējam sākumu
    
    Co-Authored-By: AnastasijaSinicina <129928532+AnastasijaSinicina@users.noreply.github.com>

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/style_main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="assets/script.js" defer></script>
     <title>Apskati Latviju</title>
 </head>
@@ -11,17 +12,16 @@
 <header>
         <a href="#" class="logo"><img src="images/latvia.png" alt=""></a>
         <nav class="navbar">
-            <a href="#piedavajumi">Piedāvājumi</a>
-            <a href="#populari">Populārakas vietas</a>
-            <a href="#jaunumi">Aktualitātes</a>
             <a href="#parmums">Par mums</a>
+            <a href="#jaunumi">Aktualitātes</a>
+            <a href="#piedavajumi">Piedāvājumi</a>       
             <a href="#kontakti">Kontakti</a> 
         </nav>
     </header>
 
-    <section id="slideshow">
+    <section id="parmums">
     <div class="welcom">
-    <p>Esat sveicināti</p>
+    <p>Esat sveicināti!</p>
     <h1>Tūrisma aģentūrā <span>Apskati Latviju</span></h1>
     <p>Jau vairākus gadus mūsu uzņēmums izstrādā tūrisma maršrutus Latvijā.<br>Apskatiet mūsu populārākos maršrutu piedāvājumus un izbaudiet Latviju kopā ar mums!</p>
 </div>
@@ -31,11 +31,51 @@
         </video>
     </div>
 </section>
-  <!-- <img class="photo active" src="./images/latvia1.jpg">
-    <img class="photo" src="./images/latvia2.jpg">
-    <img class="photo" src="./images/latvia3.jpg">
-    <img class="photo" src="./images/latvia4.jpg">
-    <img class="photo" src="./images/latvia6.jpg"> -->
 
+<section id="kontakti">
+    <div class="kontform">
+    <h1><span>Kontakti</span></h1>
+    <div class="box-container">
+    <div class="icon">
+       <i class="fas fa-phone"></i><br>
+       <h3>Tālrunis</h3>
+       <p>+371 67 777 777</p>
+       <p>+371 61 111 111</p>
+    </div>
+    <div class="icon">
+       <i class="fas fa-envelope"></i><br>
+       <h3>E-pasts</h3>
+       <p>apskatiLatviju@gmail.com</p>
+    </div>
+    <div class="icon">
+       <i class="fas fa-map-marker-alt"></i><br>
+       <h3>Adrese</h3>
+       <p>kakaja ta iela 37 <br> Liepāja, LV-4305, Latvija</p>
+    </div>
+   </div>
+
+   <div class="row">
+   
+
+    <form action = "" method = "post">
+        <div class="box-container">
+        <input type="text" name="vards" placeholder="Vārds" class="box" required>
+        <input type="email" name="epasts" placeholder="E-pasts" class="box" required>
+        <input type="tel" name="talrunis" placeholder="Tālrunis" class="box" required>
+        </div>
+        <textarea name="zinojums" placeholder="Tava ziņa" class="box" required></textarea>
+        <button type="submit" class="btn" name="nosutit">Sazināties</button>
+        <!-- <?php
+        require 'assets/mail.php';
+        ?> -->
+    </form>
+   </div>
+    </form>
+   </div>
+</section>
+
+<footer>
+<iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d17616.429411658326!2d21.00551208438658!3d56.50128502145561!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46faa6100b75a58f%3A0xb6051730e87a9a9a!2sPu%C4%B7u%20Bode!5e0!3m2!1sru!2slv!4v1715439050285!5m2!1sru!2slv" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</footer>
 </body>
 </html>
