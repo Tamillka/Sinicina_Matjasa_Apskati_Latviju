eb942210ba9af9191031c7ed70585c62199b44e0
Tamillka <tamila.matjasha@gmail.com>
2024-05-12 23:54:20 +0300

    Footer
    Izveidots footer, pārcelti kontakti uz citu lapu, piekoriģēta sākuma sadaļa, atseviški sadalīdi header un footer ērtākajam darbam

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style_main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="assets/script.js" defer></script>
+    <title>Apskati Latviju</title>
+</head>
+<body>
+<header>
+        <a href="#" class="logo"><img src="images/latvia.png" alt=""></a>
+        <nav class="navbar">
+            <a href="#parmums">Par mums</a>
+            <a href="#jaunumi">Aktualitātes</a>
+            <a href="#piedavajumi">Piedāvājumi</a>       
+            <a href="kontakti.php">Kontakti</a> 
+        </nav>
+    </header>
---
+++ kontakti.php
@@ -0,0 +1,46 @@
+<?php 
+require "header.php";
+?>
+<section id="kontakti">
+    <div class="kontform">
+    <h1><span>Kontakti</span></h1>
+    <div class="box-container">
+    <div class="icon">
+       <i class="fas fa-phone"></i><br>
+       <h3>Tālrunis</h3>
+       <p>+371 67 777 111</p>
+       <p>+371 24 555 761</p>
+    </div>
+    <div class="icon">
+       <i class="fas fa-envelope"></i><br>
+       <h3>E-pasts</h3>
+       <p>apskatiLatviju@gmail.com</p>
+    </div>
+    <div class="icon">
+       <i class="fas fa-map-marker-alt"></i><br>
+       <h3>Adrese</h3>
+       <p>kakaja ta iela 37 <br> Liepāja, LV-4305, Latvija</p>
+    </div>
+   </div>
+
+   <div class="row">
+    <form action = "" method = "post">
+        <div class="box-container">
+        <input type="text" name="vards" placeholder="Vārds" class="box" required>
+        <input type="email" name="epasts" placeholder="E-pasts" class="box" required>
+        <input type="tel" name="talrunis" placeholder="Tālrunis" class="box" required>
+        </div>
+        <textarea name="zinojums" placeholder="Tava ziņa" class="box" required></textarea>
+        <button type="submit" class="btn" name="nosutit">Sazināties</button>
+        <!-- <?php
+        require 'assets/mail.php';
+        ?> -->
+    </form>
+   </div>
+    </form>
+   </div>
+</section>
+
+<?php 
+require "footer.php";
+?>
